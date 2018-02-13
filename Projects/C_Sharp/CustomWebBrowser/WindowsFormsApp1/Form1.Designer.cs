e0f933cf3f9d66069d064dd1dcc9e38c21e2a9ce
Steven Leskin <StevenLeskin@gmail.com>
2018-02-12 16:46:22 -0800

    Add files via upload

---
+++ Projects/C_Sharp/CustomWebBrowser/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BackBTN = new System.Windows.Forms.ToolStripButton();
+            this.forwardBTN = new System.Windows.Forms.ToolStripButton();
+            this.searchBX = new System.Windows.Forms.ToolStripComboBox();
+            this.goBTN = new System.Windows.Forms.ToolStripButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BackBTN,
+            this.forwardBTN,
+            this.searchBX,
+            this.goBTN});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1442, 28);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BackBTN
+            // 
+            this.BackBTN.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.BackBTN.Image = ((System.Drawing.Image)(resources.GetObject("BackBTN.Image")));
+            this.BackBTN.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BackBTN.Name = "BackBTN";
+            this.BackBTN.Size = new System.Drawing.Size(50, 25);
+            this.BackBTN.Text = "BACK";
+            this.BackBTN.Click += new System.EventHandler(this.BackBTN_Click);
+            // 
+            // forwardBTN
+            // 
+            this.forwardBTN.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.forwardBTN.Image = ((System.Drawing.Image)(resources.GetObject("forwardBTN.Image")));
+            this.forwardBTN.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.forwardBTN.Name = "forwardBTN";
+            this.forwardBTN.Size = new System.Drawing.Size(83, 25);
+            this.forwardBTN.Text = "FORWARD";
+            this.forwardBTN.Click += new System.EventHandler(this.forwardBTN_Click);
+            // 
+            // searchBX
+            // 
+            this.searchBX.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
+            this.searchBX.Name = "searchBX";
+            this.searchBX.Size = new System.Drawing.Size(121, 28);
+            this.searchBX.Text = "SEARCH";
+            this.searchBX.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchBX_KeyDown);
+            // 
+            // goBTN
+            // 
+            this.goBTN.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.goBTN.Image = ((System.Drawing.Image)(resources.GetObject("goBTN.Image")));
+            this.goBTN.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.goBTN.Name = "goBTN";
+            this.goBTN.Size = new System.Drawing.Size(34, 25);
+            this.goBTN.Text = "GO";
+            this.goBTN.Click += new System.EventHandler(this.goBTN_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.ContextMenuStrip = this.contextMenuStrip1;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 28);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1442, 496);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1442, 524);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Custom Web Browser";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BackBTN;
+        private System.Windows.Forms.ToolStripButton forwardBTN;
+        private System.Windows.Forms.ToolStripComboBox searchBX;
+        private System.Windows.Forms.ToolStripButton goBTN;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+    }
+}
+
---
+++ Projects/C_Sharp/CustomWebBrowser/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchBX_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                webBrowser1.Navigate(searchBX.Text);
+            }
+
+        }
+
+        private void BackBTN_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void forwardBTN_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void goBTN_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(searchBX.Text);
+        }
+
+         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Text = "Custom Web Brower- " + webBrowser1.Document.Title;
+        }
+    }
+}
